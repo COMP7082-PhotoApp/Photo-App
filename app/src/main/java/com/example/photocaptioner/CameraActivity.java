96edde76d8ada47e2f93140622c0154dd9f2cf57
William Bradshaw <williambradshaw604@gmail.com>
2019-10-02 11:30:07 -0700

    comments added to CameraActivity

--- app/src/main/java/com/example/photocaptioner/CameraActivity.java
+++ app/src/main/java/com/example/photocaptioner/CameraActivity.java
@@ -21,12 +21,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 // William Bradshaw
-// CameraActivity - Takes photo when button pressed, and adds a timestamp automatically
+// CameraActivity - Launches Android's built-in camera to take a photo when button pressed.
 
 public class CameraActivity extends AppCompatActivity {
 
     String currentPhotoPath = null;
-    ExifInterface exif;
 
     static final int REQUEST_TAKE_PHOTO = 1;
 
@@ -36,30 +35,34 @@ public class CameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
     }
 
+    /** onClick function for button to take Photo */
     public void takePhoto(View v) throws IOException {
 
         dispatchTakePictureIntent();
 
     }
 
+    /** Creates files/Download if it does not exist, then creates jpg file for new photo */
     private File newImageFile() throws IOException{
 
-        //Check to see if "Download" folder does not exist
+        //Create file path to Download folder to check if it exists and later generate a URI
 
         File localDirectory = getFilesDir();
         File downloadFolder = new File(localDirectory, "Download");
 
+        //Create the Download folder if it does not exist
         if(!downloadFolder.exists()) {
             downloadFolder.mkdir();
         }
 
 
-        Log.i("Message", "attempt to make new image file");
+        //Log.i("Message", "attempt to make new image file");
 
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        // Prepares the final path for the output photo file
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -69,42 +72,37 @@ public class CameraActivity extends AppCompatActivity {
         // Save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
 
-        Log.i("Photo Path", currentPhotoPath);
-
-        showTimestamp(currentPhotoPath);
+        //Log.i("Photo Path", currentPhotoPath);
 
         return image;
 
     }
 
-    private void showTimestamp(String selectedPhoto){
-        try {
-            ExifInterface e = new ExifInterface(selectedPhoto);
-            Toast toast = Toast.makeText(CameraActivity.this, "CAPTION: " + e.getAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION) +
-                    "\n DATE: " + e.getAttribute(ExifInterface.TAG_DATETIME), Toast.LENGTH_LONG);
-            TextView textView = (TextView) toast.getView().findViewById(android.R.id.message);
-            textView.setTextColor(Color.RED);
-            toast.show();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    // Example from Android tutorial
+    /** Launches Android's built-in camera activity */
     private void dispatchTakePictureIntent() throws IOException {
 
-
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Check if Android camera can be successfully accessed by the app
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
 
+            // Creates an image file to store the new photo
             File newPhoto = newImageFile();
 
+            // Check if creation of a new photo file was successful
             if (newPhoto != null) {
 
+                // Gets a URI for the file based on authority of app's own fileprovider
                 Uri photoURI = FileProvider.getUriForFile(this,
                         "com.example.photocaptioner.fileprovider",
                         newPhoto);
+
+                // Creates takePictureIntent with two extra parameters
+                // MediaStore.EXTRA_OUTPUT indicates the the following photoURI will store the photo
+
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                // Starts the Android Camera activity
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
 
             }
