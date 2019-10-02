c3b2016dd79a4a4b69497bdf35c79096a34a50db
William Bradshaw <williambradshaw604@gmail.com>
2019-10-01 22:47:43 -0700

    restarted work on camera activity

---
+++ app/src/main/java/com/example/photocaptioner/CameraActivity.java
@@ -0,0 +1,88 @@
+package com.example.photocaptioner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// William Bradshaw
+// CameraActivity - Takes photo when button pressed, and adds a timestamp automatically
+
+public class CameraActivity extends AppCompatActivity {
+
+    String currentPhotoPath = null;
+    ExifInterface exif;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+
+    public void takePhoto(View v) throws IOException {
+
+        dispatchTakePictureIntent();
+
+    }
+
+    private File newImageFile() throws IOException{
+
+        Log.e("Message", "attempt to make new image file");
+
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+
+        Log.e("Photo Path", currentPhotoPath);
+
+        return image;
+
+    }
+
+    // Example from Android tutorial
+    private void dispatchTakePictureIntent() throws IOException {
+
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            File newPhoto = newImageFile();
+
+            if (newPhoto != null) {
+
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        newPhoto);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+            }
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/photocaptioner/MainActivity.java
+++ app/src/main/java/com/example/photocaptioner/MainActivity.java
@@ -15,6 +15,7 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
@@ -113,6 +114,13 @@ public class MainActivity extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
             requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
+    }
+
+    /** function to start activity for camera portion of app */
+
+    public void openCameraScreen(View v){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
     }
 
     /** function for dialog of alert */
